2928f5f10ae38f2bb50d8c91b50d9997923be0db
Kiwi Bui <kiwibui4@gmail.com>
2021-05-07 09:01:36 -0700

    initializr

---
+++ src/main/java/com/galvanize/ajkiwigames/AjKiwiGamesApplication.java
@@ -0,0 +1,13 @@
+package com.galvanize.ajkiwigames;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AjKiwiGamesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AjKiwiGamesApplication.class, args);
+	}
+
+}
